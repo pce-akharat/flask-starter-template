c9da2d3d5ef1f34cf7b89f61e203362ca16ca6e2
Amol Kharat <akharat@mes.ac.in>
2019-09-17 14:05:13 +0530

    Links updated

--- app.py
+++ app.py
@@ -3,8 +3,13 @@ from flask import Flask, url_for, render_template
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/home')
 def index():
     return render_template('index.html', title="Home")
+
+@app.route('/aboutus')
+def aboutus():
+    return render_template('aboutus.html', title="About Us")
 
 if __name__ == "__main__":
     app.run(debug=True)
